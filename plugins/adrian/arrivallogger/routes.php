14b6f4d45bc7fc59bfa686d4851a897306984449
Adrian Dvorsky <dvorsky@wezeo.com>
2022-10-22 18:59:57 +0200

    http controller

--- plugins/adrian/arrivallogger/routes.php
+++ plugins/adrian/arrivallogger/routes.php
@@ -1,19 +1,28 @@
 <?php
 
-use Adrian\Arrivallogger\Models\Arrivallogger;
-use Adrian\Arrivallogger\Http\Resources\ArrivalloggerResource;
+use Adrian\Arrivallogger\Http\Controllers\ArrivalloggerController;
+// use Adrian\Arrivallogger\Models\Arrivallogger;
+// use Adrian\Arrivallogger\Http\Resources\ArrivalloggerResource;
+
+Route::get('students-arrival', [ArrivalloggerController::class, 'arrivals']);
 
 
+/*
 Route::group(['prefix' => 'resource'], function() {
-    Route::get('student-arrival/{id}', function ($id) {
-        return new ArrivalloggerResource(Arrivallogger::find($id));
-    });
-
     Route::get('students-arrival', function () {
         return ArrivalloggerResource::collection(Arrivallogger::all());
     });
-});
+    Route::get('student-arrival/{id}', function ($id) {
+        return new ArrivalloggerResource(Arrivallogger::find($id));
+    });
+    Route::get('orm1/{name}', function ($name) {
+        return new ArrivalloggerResource(Arrivallogger::find($name));
+    });
 
+});
+*/
+
+/*
 Route::group(['prefix' => 'api'], function() {
     Route::get('students-arrival', function () {
         return Arrivallogger::all();
@@ -41,4 +50,4 @@ Route::group(['prefix' => 'api'], function() {
         return $log;
     });
 });
-
+*/
