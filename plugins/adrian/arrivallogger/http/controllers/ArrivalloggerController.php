62360989ae06398bc0f61bcad552d51609ee8212
Adrian Dvorsky <dvorsky@wezeo.com>
2022-10-23 20:33:37 +0200

    http controller + resource

--- plugins/adrian/arrivallogger/http/controllers/ArrivalloggerController.php
+++ plugins/adrian/arrivallogger/http/controllers/ArrivalloggerController.php
@@ -4,12 +4,33 @@ namespace Adrian\Arrivallogger\Http\Controllers;
  
 use Adrian\Arrivallogger\Http\Resources\ArrivalloggerResource;
 use Adrian\Arrivallogger\Models\Arrivallogger;
+use Illuminate\Routing\Controller;
 
-class ArrivalloggerController
+class ArrivalloggerController extends Controller
 {
 
-    public function Arrivals()
+    public function arrivals()
     {
         return ArrivalloggerResource::collection(Arrivallogger::all());
     }
+
+    public function arrival($name)
+    {
+        return ArrivalloggerResource::collection(Arrivallogger::where('name', 'LIKE', $name)->get());
+    }
+
+    public function orderArrivals()
+    {
+        return ArrivalloggerResource::collection(Arrivallogger::orderBy('created_at', 'desc')->get());
+    }
+
+    public function filterArrival($name, $late)
+    {
+        return ArrivalloggerResource::collection(Arrivallogger::where('name', 'LIKE', $name)
+            ->orWhere('late', 'LIKE', $late)
+            ->get());
+    }
+
 }
+
+
--- plugins/adrian/arrivallogger/routes.php
+++ plugins/adrian/arrivallogger/routes.php
@@ -6,48 +6,9 @@ use Adrian\Arrivallogger\Http\Controllers\ArrivalloggerController;
 
 Route::get('students-arrival', [ArrivalloggerController::class, 'arrivals']);
 
+Route::get('student-arrival/{name}', [ArrivalloggerController::class, 'arrival']);
 
-/*
-Route::group(['prefix' => 'resource'], function() {
-    Route::get('students-arrival', function () {
-        return ArrivalloggerResource::collection(Arrivallogger::all());
-    });
-    Route::get('student-arrival/{id}', function ($id) {
-        return new ArrivalloggerResource(Arrivallogger::find($id));
-    });
-    Route::get('orm1/{name}', function ($name) {
-        return new ArrivalloggerResource(Arrivallogger::find($name));
-    });
+Route::get('order-arrivals', [ArrivalloggerController::class, 'orderArrivals']);
 
-});
-*/
+Route::get('filter-arrival/{name}/{late}', [ArrivalloggerController::class, 'filterArrival']);
 
-/*
-Route::group(['prefix' => 'api'], function() {
-    Route::get('students-arrival', function () {
-        return Arrivallogger::all();
-    });
-    
-    Route::post('add-arrival', function () {
-        Arrivallogger::create(post());
-        return 'Arrival added';
-    });
-    
-    Route::get('orm1/{name}', function ($name) {
-        $log = Arrivallogger::where('name', 'LIKE', $name)->get();
-        return $log;
-    });
-    
-    Route::get('orm2/{name}/{late}', function ($name, $late) {
-        $log = Arrivallogger::where('name', 'LIKE', $name)
-            ->orWhere('late', 'LIKE', $late)
-            ->get();
-        return $log;
-    });
-    
-    Route::get('orm3', function () {
-        $log = Arrivallogger::orderBy('created_at', 'desc')->get();
-        return $log;
-    });
-});
-*/
