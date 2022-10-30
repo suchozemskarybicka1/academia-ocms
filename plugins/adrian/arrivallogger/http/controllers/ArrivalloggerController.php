d550056cfb7b57103f85aee447ffe0885f38be86
Adrian Dvorsky <dvorsky@wezeo.com>
2022-10-30 21:04:20 +0100

    precistenie routes a htt controller

--- plugins/adrian/arrivallogger/http/controllers/ArrivalloggerController.php
+++ plugins/adrian/arrivallogger/http/controllers/ArrivalloggerController.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Auth;
 use Adrian\Arrivallogger\Http\Resources\ArrivalloggerResource;
 use Adrian\Arrivallogger\Models\Arrivallogger;
 use Illuminate\Routing\Controller;
-use LibUser\Userapi\Http\Resources\UserResource;
-use LibUser\Userapi\Models\User;
 
 
 class ArrivalloggerController extends Controller
@@ -15,29 +13,8 @@ class ArrivalloggerController extends Controller
 
     public function index()
     {
-        return ArrivalloggerResource::collection(Arrivallogger::all());
-    }
-
-    public function show($name)
-    {
-        return ArrivalloggerResource::collection(Arrivallogger::where('name', 'LIKE', $name)->get());
-    }
-
-    public function showOrdered()
-    {
-        return ArrivalloggerResource::collection(Arrivallogger::orderBy('created_at', 'desc')->get());
-    }
-    
-    public function showLateOrName($name, $late)
-    {
-        return ArrivalloggerResource::collection(Arrivallogger::where('name', 'LIKE', $name)
-            ->orWhere('late', 'LIKE', $late)
-            ->get());
-    }
-
-    public function indexAuthUser()
-    {
-        return Auth::user()->name;
+        $authId = Auth::user()->id;
+        return Arrivallogger::where('user_id', 'LIKE', $authId)->get();
     }
 
     public function store() 
@@ -50,5 +27,6 @@ class ArrivalloggerController extends Controller
 
         return ArrivalloggerResource::make($Arrivallogger);
     }
+
 
 }
