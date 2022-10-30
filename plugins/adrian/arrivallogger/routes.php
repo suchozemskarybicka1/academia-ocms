dfb9cce723bf4664f5b1b1c61e2b2fe928e08b23
Adrian Dvorsky <dvorsky@wezeo.com>
2022-10-30 21:05:52 +0100

    precistenie routes a http controller

--- plugins/adrian/arrivallogger/routes.php
+++ plugins/adrian/arrivallogger/routes.php
@@ -1,23 +1,12 @@
 <?php
 
 use Adrian\Arrivallogger\Http\Controllers\ArrivalloggerController;
-use Adrian\Arrivallogger\Models\Arrivallogger;
-use LibUser\Userapi\Models\User;
-// use Adrian\Arrivallogger\Http\Resources\ArrivalloggerResource;
-use LibUser\Userapi\Http\Resources\UserResource;
 
-Route::get('students-arrival', [ArrivalloggerController::class, 'index']);
-
-Route::get('student-arrival/{name}', [ArrivalloggerController::class, 'show']);
-
-Route::get('order-arrivals', [ArrivalloggerController::class, 'showOrdered']);
-
-Route::get('filter-arrival/{name}/{late}', [ArrivalloggerController::class, 'showLateOrName']);
 
 Route::middleware(['auth'])->group (function() {
+    
+    Route::get('my-arrivals', [ArrivalloggerController::class, 'index']);    
   
-    Route::get('get-auth-user', [ArrivalloggerController::class, 'indexAuthUser']);    
-  
-    Route::post('add-user-id', [ArrivalloggerController::class, 'store']);
-
+    Route::post('new-arrival', [ArrivalloggerController::class, 'store']);
+    
 });
